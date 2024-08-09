66af2a003ae501dec379cc76b1195bed3a6db344
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2024-08-09 17:47:13 +0530

    MyPow

---
+++ LeetCode/MyPow.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+    return x ** n;
+};
+
+let x = 2;
+let n = 10;
+console.log(myPow(x, n));
+
+x = 2.1;
+n = 3;
+console.log(myPow(x, n));
+
