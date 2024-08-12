d42e99e09c951f1e997c3e9dc7a9053546abb50d
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2024-08-12 22:33:33 +0530

    maxDepth

---
+++ LeetCode/maxDepth.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+var maxDepth = function (root) {
+    return root === null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+let root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
+console.log(maxDepth(root)); // Output: 3
