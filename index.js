8ad34e323cd6e51571e01850d9f9ccccec084d67
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2024-08-21 22:17:07 +0530

    practice

--- index.js
+++ index.js
@@ -302,6 +302,25 @@ let result = ''
 
 // console.log(036 / 1);
 
-console.log(Number((" ")));
+// console.log(Number((" ")));
 
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+
+console.log(head);
+
+let dummy = head;
+console.log(dummy);
+
+dummy.next = new ListNode(5);
+console.log(dummy);
+console.log(head);
 
