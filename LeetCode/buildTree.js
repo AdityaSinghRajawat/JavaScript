309d81a75723a910a524636d3db68ee65aa572f3
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2024-08-22 15:26:28 +0530

    buildTree

---
+++ LeetCode/buildTree.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+
+var buildTree = function (preorder, inorder) {
+
+    if (!preorder.length || !inorder.length) return null;
+
+    let rootVal = preorder[0];
+    let root = new TreeNode(rootVal);
+
+    let mid = inorder.indexOf(rootVal);
+
+    root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+    root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+
+    return root;
+
+};
+
+let preorder = [3, 9, 20, 15, 7];
+let inorder = [9, 3, 15, 20, 7];
+console.log(buildTree(preorder, inorder));
+// Output: [3, 9, 20, null, null, 15, 7]
